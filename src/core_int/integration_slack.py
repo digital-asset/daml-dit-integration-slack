fe42a306fafaf0fc37ee2afd4cd4afb48a49e621
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2021-07-06 14:17:35 -0400

    Update to latest build tool conventions.

--- src/core_int/integration_slack.py
+++ src/core_int/integration_slack.py
@@ -10,12 +10,15 @@ from dazl.model.core import ContractData
 from slack import WebClient
 
 from daml_dit_if.api import \
-    IntegrationEnvironment, IntegrationEvents, IntegrationWebhookResponse
+    getIntegrationLogger, \
+    IntegrationEnvironment, \
+    IntegrationEvents, \
+    IntegrationWebhookResponse
 
 from daml_dit_if.main.web import json_response
 
 
-LOG = logging.getLogger('integration')
+LOG = getIntegrationLogger()
 
 
 @dataclass
@@ -32,7 +35,7 @@ def integration_slack_main(
     @events.ledger.contract_created(
         'SlackIntegration.OutboundMessage:OutboundMessage')
     async def on_contract_created(event):
-        LOG.info('slack send message - created: %r', event)
+        LOG.debug('Outbound message contract created: %r', event)
 
         channel = event.cdata['slackChannel']
         text = event.cdata['messageText']
@@ -45,19 +48,22 @@ def integration_slack_main(
     async def on_webhook_post(request):
         body = await request.json()
 
+        LOG.debug('Webhook event received: %r', body)
+
         if body['type'] == 'url_verification':
             LOG.info('URL Verification Challenge: %r', body['challenge'])
 
             return IntegrationWebhookResponse(
                 response=json_response({'challenge': body['challenge']}))
 
-        if body['type'] == 'event_callback' and body['event']['type'] == 'message' \
+        if body['type'] == 'event_callback' \
+           and body['event']['type'] == 'message' \
            and body['event'].get('subtype') != 'bot_message':
 
             LOG.debug('Inbound Message: %r', body)
 
             return IntegrationWebhookResponse(
-                commands=[create('SlackIntegration.InboundDirectMessage:InboundDirectMessage', {
+                commands=[create(env.tid('SlackIntegration.InboundDirectMessage:InboundDirectMessage'), {
                     'integrationParty': env.party,
                     'slackChannel': body['event']['channel'],
                     'slackUser': body['event']['user'],
