b677c14acb49039a92fd48599b0c8225449c13a8
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2021-07-07 06:50:49 -0400

    Map inbound slash command invocations into contract create events.

--- src/core_int/integration_slack.py
+++ src/core_int/integration_slack.py
@@ -37,7 +37,6 @@ def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEv
 
         return [exercise(event.cid, 'Archive')]
 
-    @events.webhook.post(label='Slack Event Webhook Endpoint')
     async def on_webhook_post(request):
         body = await request.json()
 
@@ -65,3 +64,39 @@ def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEv
 
         LOG.debug('Unhandled inbound event type: %r', body['type'])
         return IntegrationWebhookResponse()
+
+    async def on_command_invocation(request):
+        LOG.info('Inbound Command Invocation: %r', request)
+
+        body = await request.post()
+
+        LOG.info('Body Text: %r', body)
+
+        for k in body.keys():
+            LOG.info('   %r => %r', k, body.getall(k, '== DEFAULT =='))
+
+        return IntegrationWebhookResponse(
+            commands=[create(env.tid('SlackIntegration.Commands:CommandInvocation'), {
+                'integrationParty': env.party,
+                'command': body.getone('command', None),
+                'commandText': body.getone('text', None),
+                'responseUrl': body.getone('response_url', None),
+                'slackUser': body.getone('user_id', None),
+                'slackTeam': body.getone('team_id', None),
+                'slackChannel': body.getone('channel_id', None),
+                'slackApiAppId': body.getone('api_app_id', None)
+            })])
+
+    @events.webhook.post(label='Slack Webhook Endpoint - Events and Commands')
+    async def on_http_post(request):
+
+        LOG.info('Inbound request: %r', request)
+
+        content_type = request.content_type
+
+        LOG.info('Inbound request content type: %r', content_type)
+
+        if content_type == "application/x-www-form-urlencoded":
+            return await on_command_invocation(request)
+        else:
+            return await on_webhook_post(request)
