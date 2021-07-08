239c7dd379c9889f7ec53cca3733e752c3afffc4
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2021-07-08 07:35:00 -0400

    Provide a DAML mechanism for replying to inbound command invocations.

--- src/core_int/integration_slack.py
+++ src/core_int/integration_slack.py
@@ -1,25 +1,31 @@
 # Copyright (c) 2020, Digital Asset (Switzerland) GmbH and/or its affiliates.
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
+import datetime
 
 from dataclasses import dataclass
+
+from aiohttp import ClientSession
 
 from dazl import create, exercise
 from dazl.model.core import ContractData
 
 from daml_dit_if.api import \
+    empty_success_response, \
+    json_response, \
     getIntegrationLogger, \
     IntegrationEnvironment, \
     IntegrationEvents, \
     IntegrationWebhookResponse
 
-from daml_dit_if.main.web import json_response
-
 from .slack_client import connect_slack_client
 
 
 LOG = getIntegrationLogger()
+
+
+def current_time():
+    return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEvents'):
@@ -36,6 +42,24 @@ def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEv
         sc().chat_postMessage(channel=channel, text=text)
 
         return [exercise(event.cid, 'Archive')]
+
+    @events.ledger.contract_created('SlackIntegration.Commands:CommandInvocationResponse')
+    async def on_command_response(event):
+        LOG.debug('Command invocation response requested: %r', event)
+
+        async with ClientSession() as session:
+            async with session.post(event.cdata['responseUrl'], json={
+                    'text': event.cdata['responseText']
+            }) as resp:
+
+                if resp.status == 200:
+                    return [exercise(event.cid, 'Archive')]
+                else:
+                    return [exercise(event.cid, 'ResponseFailed', {
+                        'receivedAt': current_time(),
+                        'httpStatusCode': resp.status,
+                        'httpResponseBody': await resp.text()
+                    })]
 
     async def on_webhook_post(request):
         body = await request.json()
@@ -57,27 +81,28 @@ def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEv
             return IntegrationWebhookResponse(
                 commands=[create(env.tid('SlackIntegration.Messages:InboundDirectMessage'), {
                     'integrationParty': env.party,
+                    'receivedAt': current_time(),
                     'slackChannel': body['event']['channel'],
                     'slackUser': body['event']['user'],
                     'messageText': body['event']['text'],
                 })])
 
-        LOG.debug('Unhandled inbound event type: %r', body['type'])
+        LOG.warn('Unhandled inbound event type from Slack: %r', body['type'])
+
         return IntegrationWebhookResponse()
 
     async def on_command_invocation(request):
-        LOG.info('Inbound Command Invocation: %r', request)
+        LOG.debug('Inbound Command Invocation: %r', request)
 
         body = await request.post()
 
-        LOG.info('Body Text: %r', body)
-
-        for k in body.keys():
-            LOG.info('   %r => %r', k, body.getall(k, '== DEFAULT =='))
+        LOG.debug('Command body: %r', body)
 
         return IntegrationWebhookResponse(
+            response=empty_success_response(),
             commands=[create(env.tid('SlackIntegration.Commands:CommandInvocation'), {
                 'integrationParty': env.party,
+                'receivedAt': current_time(),
                 'command': body.getone('command', None),
                 'commandText': body.getone('text', None),
                 'responseUrl': body.getone('response_url', None),
@@ -89,12 +114,14 @@ def integration_slack_main(env: 'IntegrationEnvironment', events: 'IntegrationEv
 
     @events.webhook.post(label='Slack Webhook Endpoint - Events and Commands')
     async def on_http_post(request):
-
-        LOG.info('Inbound request: %r', request)
+        LOG.debug('Inbound HTTP request: %r', request)
 
         content_type = request.content_type
 
-        LOG.info('Inbound request content type: %r', content_type)
+        LOG.debug('Inbound request content type: %r', content_type)
+
+        # Slack sends command invocations using a different content
+        # type than the usual JSON payload used for webhook posts.
 
         if content_type == "application/x-www-form-urlencoded":
             return await on_command_invocation(request)
